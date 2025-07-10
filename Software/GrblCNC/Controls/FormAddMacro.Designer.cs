22d8a86dc93c40ff042d4dd4a260b12ceca45c7e
shai seger <shaise@gmail.com>
2025-07-10 10:09:28 +0300

    add missing files

---
+++ Software/GrblCNC/Controls/FormAddMacro.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace GrblCNC.Controls
+{
+    partial class FormAddMacro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboFuncKey = new System.Windows.Forms.ComboBox();
+            this.textDescription = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textGCode = new System.Windows.Forms.TextBox();
+            this.buttUpdate = new System.Windows.Forms.Button();
+            this.buttCancel = new System.Windows.Forms.Button();
+            this.buttClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Macro function key:";
+            // 
+            // comboFuncKey
+            // 
+            this.comboFuncKey.FormattingEnabled = true;
+            this.comboFuncKey.Items.AddRange(new object[] {
+            "F1",
+            "F2",
+            "F3",
+            "F4",
+            "F5",
+            "F6",
+            "F7",
+            "F8",
+            "F9"});
+            this.comboFuncKey.Location = new System.Drawing.Point(160, 12);
+            this.comboFuncKey.Name = "comboFuncKey";
+            this.comboFuncKey.Size = new System.Drawing.Size(121, 21);
+            this.comboFuncKey.TabIndex = 1;
+            this.comboFuncKey.SelectedIndexChanged += new System.EventHandler(this.comboFuncKey_SelectedIndexChanged);
+            // 
+            // textDescription
+            // 
+            this.textDescription.Location = new System.Drawing.Point(91, 39);
+            this.textDescription.Name = "textDescription";
+            this.textDescription.Size = new System.Drawing.Size(190, 20);
+            this.textDescription.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Description:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "GCode lines to send:";
+            // 
+            // textGCode
+            // 
+            this.textGCode.Location = new System.Drawing.Point(15, 84);
+            this.textGCode.Multiline = true;
+            this.textGCode.Name = "textGCode";
+            this.textGCode.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textGCode.Size = new System.Drawing.Size(266, 168);
+            this.textGCode.TabIndex = 5;
+            // 
+            // buttUpdate
+            // 
+            this.buttUpdate.Location = new System.Drawing.Point(15, 260);
+            this.buttUpdate.Name = "buttUpdate";
+            this.buttUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttUpdate.TabIndex = 6;
+            this.buttUpdate.Text = "Update";
+            this.buttUpdate.UseVisualStyleBackColor = true;
+            this.buttUpdate.Click += new System.EventHandler(this.buttUpdate_Click);
+            // 
+            // buttCancel
+            // 
+            this.buttCancel.Location = new System.Drawing.Point(206, 260);
+            this.buttCancel.Name = "buttCancel";
+            this.buttCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttCancel.TabIndex = 7;
+            this.buttCancel.Text = "Cancel";
+            this.buttCancel.UseVisualStyleBackColor = true;
+            this.buttCancel.Click += new System.EventHandler(this.buttCancel_Click);
+            // 
+            // buttClear
+            // 
+            this.buttClear.Location = new System.Drawing.Point(125, 260);
+            this.buttClear.Name = "buttClear";
+            this.buttClear.Size = new System.Drawing.Size(75, 23);
+            this.buttClear.TabIndex = 8;
+            this.buttClear.Text = "Clear";
+            this.buttClear.UseVisualStyleBackColor = true;
+            this.buttClear.Click += new System.EventHandler(this.buttClear_Click);
+            // 
+            // FormAddMacro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 295);
+            this.Controls.Add(this.buttClear);
+            this.Controls.Add(this.buttCancel);
+            this.Controls.Add(this.buttUpdate);
+            this.Controls.Add(this.textGCode);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textDescription);
+            this.Controls.Add(this.comboFuncKey);
+            this.Controls.Add(this.label1);
+            this.Name = "FormAddMacro";
+            this.Text = "Add / Edit GCode Macro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboFuncKey;
+        private System.Windows.Forms.TextBox textDescription;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textGCode;
+        private System.Windows.Forms.Button buttUpdate;
+        private System.Windows.Forms.Button buttCancel;
+        private System.Windows.Forms.Button buttClear;
+    }
+}
---
+++ Software/GrblCNC/Controls/FormAddMacro.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using GrblCNC.Glutils;
+
+namespace GrblCNC.Controls
+{
+    public partial class FormAddMacro : Form
+    {
+        cMacroKey[] macros = new cMacroKey[10];
+        cMacroKey[] macrosBkp = new cMacroKey[10];
+        ToolStripButton[] buttons = new ToolStripButton[10];
+        int currentKey = 0;
+        ToolStrip toolstrip;
+        public FormAddMacro()
+        {
+            InitializeComponent();
+            
+            for (int i = 0; i < macros.Length; i++)
+            {
+                macrosBkp[i] = new cMacroKey(i);
+                macros[i] = new cMacroKey(i);
+                buttons[i] = GenerateButton(i);
+            }
+            comboFuncKey.SelectedIndex = 0;
+            LoadFromFile();
+        }
+
+        void LoadFromFile()
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(Utils.SettingPath("MacroDefs.txt"));
+                cMacroKey mac = null;
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine().Trim();
+                    if (line.StartsWith("#"))
+                        continue;
+                    if (line.StartsWith(":F"))
+                    {
+                        string [] vars = line.Split(new char[] {':'}, StringSplitOptions.RemoveEmptyEntries);
+                        int id = int.Parse(vars[0][1].ToString()) - 1;
+                        if (id >= 0 && id <= 8)
+                        {
+                            mac = macrosBkp[id];
+                            mac.description = vars[1];
+                            mac.macroText = "";
+                        }
+                        else
+                            mac = null;
+                    }
+                    else
+                    {
+                        if ((mac != null) && (line != ""))
+                        {
+                            mac.macroText += line + "\r\n";
+                        }
+                    }
+                }
+                sr.Close();
+                RestoreFunctions();
+                UpdateGui(currentKey);
+            }
+            catch
+            {
+            }
+        }
+        void SaveToFile()
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(Utils.SettingPath("MacroDefs.txt"));
+                sw.WriteLine("# GrblCNC macro definitions");
+                for (int i = 0; i < macros.Length; i++)
+                {
+                    if (macrosBkp[i].description != "")
+                    {
+                        sw.WriteLine(string.Format(":F{0}:{1}", i + 1, macrosBkp[i].description));
+                        sw.WriteLine(macrosBkp[i].macroText);
+                    }
+                }
+                sw.Close();
+            } catch 
+            {
+                Global.errHandler.AddError("Unable to save macro definitions");
+            } 
+        }
+
+        public void SetToolStrip(ToolStrip toolStrip)
+        {
+            toolstrip = toolStrip;
+            UpdateToolStrip();
+        }
+
+        public void EnableFuncs(bool isenable)
+        {
+            for (int i = 0;i < buttons.Length;i++)
+            {
+                buttons[i].Enabled = isenable;
+            }
+        }
+
+        void BackupFunctions()
+        {
+            for (int i = 0; i < macros.Length; i++)
+            {
+                macrosBkp[i].copyFrom(macros[i]);
+            }
+            SaveToFile();
+        }
+
+        void RestoreFunctions()
+        {
+            for (int i = 0; i < macros.Length; i++)
+            {
+                macros[i].copyFrom(macrosBkp[i]);
+            }
+            UpdateGui(currentKey);
+        }
+
+        void UpdateGui(int funcId)
+        {
+            textDescription.Text = macros[funcId].description;
+            textGCode.Text = macros[funcId].macroText;
+        }
+
+        void ReadGui(int funcId)
+        {
+            macros[funcId].description = textDescription.Text;
+            macros[funcId].macroText = textGCode.Text;
+        }
+
+        public void ActivateFunction(int funcId)
+        {
+            if (macrosBkp[funcId].description == "")
+                return;
+            string[] lines = macrosBkp[funcId].macroText.Split(new char [] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string line in lines)
+                Global.grblComm.PostLine(line);
+        }
+
+        private void buttClear_Click(object sender, EventArgs e)
+        {
+            macros[currentKey].Clear();
+            UpdateGui(currentKey);
+        }
+
+        private void comboFuncKey_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ReadGui(currentKey);
+            currentKey = comboFuncKey.SelectedIndex;
+            UpdateGui(currentKey);
+        }
+
+        private void buttCancel_Click(object sender, EventArgs e)
+        {
+            RestoreFunctions();
+            Hide();
+        }
+
+        void ClearToolStrip()
+        {
+            for (int i = 0; i < macros.Length; i++)
+            {
+                if (macrosBkp[i].description != "")
+                    toolstrip.Items.Remove(buttons[i]);
+            }
+        }
+
+        void UpdateToolStrip()
+        {
+            for (int i = 0; i < macros.Length; i++)
+            {
+                if (macrosBkp[i].description != "")
+                {
+                    buttons[i].ToolTipText = macrosBkp[i].description;
+                    toolstrip.Items.Add(buttons[i]);
+                }
+            }
+        }
+
+        private void buttUpdate_Click(object sender, EventArgs e)
+        {
+            ClearToolStrip();
+            ReadGui(currentKey);
+            BackupFunctions();
+            UpdateToolStrip();
+            Hide();
+        }
+
+        ToolStripButton GenerateButton(int num)
+        {
+            ToolStripButton butt = new ToolStripButton();
+            butt.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            butt.Image = (Image)global::GrblCNC.Properties.Resources.ResourceManager.GetObject(string.Format("MacroF{0}Buttt", num + 1));
+            butt.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            butt.ImageTransparentColor = System.Drawing.Color.Magenta;
+            butt.Name = "toolStripMacroF" + (num+1).ToString();
+            butt.Size = new System.Drawing.Size(44, 44);
+            butt.Text = num.ToString();
+            butt.ToolTipText = "";
+            butt.Click += macro_click;
+            return butt;
+        }
+
+        private void macro_click(object sender, EventArgs e)
+        {
+            ToolStripButton butt = (ToolStripButton)sender;
+            ActivateFunction(int.Parse(butt.Text));
+
+        }
+    }
+
+    public class cMacroKey
+    {
+        public cMacroKey(int id)
+        {
+            funcId = id;
+            description = "";
+            macroText = "";
+        }
+
+        public void copyFrom(cMacroKey other)
+        {
+            funcId = other.funcId;
+            description = other.description;
+            macroText = other.macroText;
+        }
+
+        public void Clear()
+        {
+            description = "";
+            macroText = "";
+        }
+
+        public int funcId;
+        public string description;
+        public string macroText;
+    }
+}
